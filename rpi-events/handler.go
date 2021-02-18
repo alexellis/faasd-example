0b336554d660e924ed986619178e5e8d62998f6c
Alex Ellis <alexellis2@gmail.com>
2021-02-18 12:00:34 +0000

    Update handler.go

--- rpi-events/handler.go
+++ rpi-events/handler.go
@@ -6,5 +6,5 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, your event was %d bytes", len(req))
+	return fmt.Sprintf(`<html><body>Hello, checkout <a href="https://skyline.github.com/alexellis/2020">my GitHub Skyline from 2020</a></body></html>`)
 }
